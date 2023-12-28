f609d9869f10a088c0ffbf90201921b1b5ccbc2b
tanvircancode <tanahmchw241996@gmail.com>
2023-12-28 23:55:13 +0600

    Styling updated

--- client/src/Pages/LoginPage/index.jsx
+++ client/src/Pages/LoginPage/index.jsx
@@ -1,41 +1,54 @@
-import { useState } from 'react';
-import './Login.scss'; // Import your Sass styles
+import { useState } from "react";
+import "./Login.scss";
+import { BsBag } from "react-icons/bs";
+import rightImage from "../../assets/login_img.png";
 
 function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  // Handle form submission logic here
+    // Handle form submission logic here
 
-  return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+    return (
+        <div className="login-page">
+            
+            <div className="left-section">
+                <div className="form-top-section">
+                    <BsBag />
+                    <h2>Login</h2>
+                </div>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="bottom-section">
+                        <button type="submit">Login</button>
+                        <span style={{ fontSize: 12 }}>
+                            New here? Create Account
+                        </span>
+                    </div>
+                </form>
+            </div>
+            <div className="right-section">
+                <img src={rightImage} alt="image here" />
+            </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className='bottom-section'>
-        <button type="submit">Login</button>
-        <span style={{fontSize: 12 }}>New here? Create Account</span>
-        </div>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default Login;
