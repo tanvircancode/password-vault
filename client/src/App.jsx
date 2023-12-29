d570a246bf586cfe05d94c0f7485badcb2d24898
tanvircancode <tanahmchw241996@gmail.com>
2023-12-29 22:10:25 +0600

    Register updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./Pages/Layout";
+// import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
 import Login from "./Pages/LoginPage";
+import Register from "./Pages/RegisterPage";
 
 function App() {
     return (
@@ -10,6 +11,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+
 
             </Routes>
         </>
--- client/src/Pages/RegisterPage/index.jsx
+++ client/src/Pages/RegisterPage/index.jsx
@@ -1,9 +1,9 @@
 // import { useState } from "react";
-import "./Login.scss";
-import { BsBag } from "react-icons/bs";
-import rightImage from "../../assets/login_img.png";
+import "./register.scss";
+import { BsPersonFillUp  } from "react-icons/bs";
+import rightImage from "../../assets/register.jpg";
 
-function Login() {
+function Register() {
     // const [username, setUsername] = useState("");
     // const [password, setPassword] = useState("");
 
@@ -11,10 +11,13 @@ function Login() {
 
     return (
         <div className="login-page">
+            <div className="right-section">
+                <img src={rightImage} alt="image here" />
+            </div>
             <div className="left-section">
                 <div className="form-top-section">
-                    <BsBag />
-                    <h2>Login</h2>
+                    <BsPersonFillUp  style={{fontSize: 30, marginBottom: 6}}/>
+                    <h2>Register</h2>
                 </div>
                 <form>
                     <div className="mb-3 form-input">
@@ -37,18 +40,24 @@ function Login() {
                             id="exampleInputPassword1"
                         />
                     </div>
-                    <div className="form-input">
-                        <button type="submit" className="btn btn-primary">
-                            Login
+                    <div className="mb-4 form-input">
+                        <label className="form-label">Password Hint</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                        />
+                    </div>
+                    <div className="form-input" >
+                        <button type="submit" className="btn btn-primary" style={{width:'100%'}} >
+                            Register
                         </button>
                     </div>
                 </form>
             </div>
-            <div className="right-section" style={{width:'50%'}}>  
-                <img src={rightImage} alt="image here" />
-            </div>
+            
         </div>
     );
 }
 
-export default Login;
+export default Register;
