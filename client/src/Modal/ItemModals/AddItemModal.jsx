ff90fd98ee70743720a043fc7a52782b352b55e7
tanvircancode <tanahmchw241996@gmail.com>
2024-01-02 23:48:28 +0600

    modal updated

--- client/src/Modal/ItemModals/AddItemModal.jsx
+++ client/src/Modal/ItemModals/AddItemModal.jsx
@@ -1,38 +1,25 @@
+/* eslint-disable react/prop-types */
 
 function AddItemModal({ openPopup, setOpenPopup }) {
-    
     return (
-        <div
-            className="modal-body"
-            style={{ display: openPopup ? "block" : "none" }}
-        >
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-4">.col-md-4</div>
-                    <div className="col-md-4 ms-auto">.col-md-4 .ms-auto</div>
-                </div>
-                <div className="row">
-                    <div className="col-md-3 ms-auto">.col-md-3 .ms-auto</div>
-                    <div className="col-md-2 ms-auto">.col-md-2 .ms-auto</div>
-                </div>
-                <div className="row">
-                    <div className="col-md-6 ms-auto">.col-md-6 .ms-auto</div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-9">
-                        Level 1: .col-sm-9
-                        <div className="row">
-                            <div className="col-8 col-sm-6">
-                                Level 2: .col-8 .col-sm-6
-                            </div>
-                            <div className="col-4 col-sm-6">
-                                Level 2: .col-4 .col-sm-6
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div className={`modal fade ${openPopup ? 'show' : ''}`} tabIndex="-1" role="dialog" style={{ display: openPopup ? 'block' : 'none' }} aria-hidden={!openPopup}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Add Item Modal</h5>
+              <button type="button" className="btn-close" aria-label="Close" onClick={() => setOpenPopup(false)}></button>
             </div>
+            <div className="modal-body">
+              {/* Add your modal content here */}
+              <p>This is the Add Item modal content.</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" onClick={() => setOpenPopup(false)}>Close</button>
+              {/* Add additional modal action buttons if needed */}
+            </div>
+          </div>
         </div>
+      </div>
     );
 }
 
