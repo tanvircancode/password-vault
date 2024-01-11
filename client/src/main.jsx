70957e5307dfd54863612f07e3e4aa52b15501cd
tanvircancode <tanahmchw241996@gmail.com>
2024-01-11 23:55:33 +0600

    redux added and some portion updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,10 +7,21 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+// redux code
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+
+export const store = configureStore({
+    reducer: {},
+  })
+
+//end
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
+         <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
